c2fc120ac638db4f299813a83d77cdc26a4256de
LoganJones <logan.b.jones16@gmail.com>
2020-12-21 17:44:04 -0800

    Update Theme colors

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6ec6ff',
+      main: '#2196f3',
+      dark: '#0069c0',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#66ffa6',
+      main: '#00e676',
+      dark: '#00b248',
+      contrastText: '#000',
+    },
+  },
   typography: {
     fontFamily: ['"Consolas"', 'Open Sans'].join(',')
-    }
+    },
 })
 
 export default theme;
