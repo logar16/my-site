f883facdfc5b4d485e2d14d6ef7131cfa98d8dfc
LoganJones <logan.b.jones16@gmail.com>
2020-12-22 22:47:09 -0800

    Update text portions of Home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Paper, Typography, Divider } from "@material-ui/core";
+import { Grid, Typography, Divider } from "@material-ui/core";
 import { Parallax } from "react-parallax";
 
 import portrait from './profile.jpg';
@@ -17,7 +17,7 @@ const useStyles = makeStyles({
     margin: 30,
     // width: 100
   },
-  pitch: {
+  longText: {
     maxWidth: 800,
   },
   logos: {
@@ -45,7 +45,7 @@ export default function Home() {
         <Divider  className={classes.divider} variant='middle'></Divider>
         <Grid container justify="center">
           <Grid item>
-            <Typography className={classes.pitch} variant="body1">{elevatorPitch}</Typography>
+            <Typography className={classes.longText} variant="body1">{elevatorPitch}</Typography>
           </Grid>
         </Grid>
         <Divider  className={classes.divider} variant='middle'></Divider>
@@ -61,10 +61,11 @@ export default function Home() {
           <ExperienceRow  image={cgm} experience={cgmExperience}/>
         </Grid>
         <Divider  className={classes.divider} variant='middle'></Divider>
-        <Typography>
-          Check out the other sections of the site to learn about my education and experience,
-          ongoing projects, and more general information about me and how I got here.
-        </Typography>
+        <Grid container justify="center">
+          <Grid item>
+            <Typography className={classes.longText} variant="body1">{invite}</Typography>
+          </Grid>
+        </Grid>
         <Divider  className={classes.divider} variant='middle'></Divider>
         <div>
           Contact info
@@ -102,26 +103,29 @@ function ExperienceRow({image, experience}) {
 
 function ExperienceText(props) {
   let experience = props.children;
-  let title = experience.title;
-  let subtitle = experience.subtitle;
-  let details = experience.details;
 
   return (
     <Grid container direction="column">
       <Grid item>
-        <Typography variant="h4">{title}</Typography>
+        <Typography variant="h4">
+          {experience.title}
+        </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="h6" style={{whiteSpace: "pre-line"}}>{subtitle}</Typography>
+        <Typography variant="h6" style={{whiteSpace: "pre-line"}}>
+          {experience.subtitle}
+        </Typography>
       </Grid>
       <Grid item>
-        <Typography variant="h6">{details}</Typography>
+        <Typography variant="h6">
+          {experience.details}
+        </Typography>
       </Grid>
     </Grid>
   );
 }
 
-const elevatorPitch = `My name is Logan Jones. I'm a self-taught programmer with a Master’s degree in 
+const elevatorPitch = `Hello! My name is Logan Jones. I'm a self-taught programmer with a Master’s degree in 
 Artificial Intelligence and Machine Learning and over four years of 
 work experience as a Software Engineer. 
 I'm trying to break into a new role as a ML researcher and engineer.  I love
@@ -150,3 +154,8 @@ const cgmExperience = {
   subtitle: "Software Engineer",
   details: "April 2016 - July 2019"
 }
+
+
+const invite = `Check out the other sections of the site to learn more about my education, experience,
+ongoing projects, and general information about me and how I got here.
+You can also find ways to contact me below. I'm always happy to connect!`
