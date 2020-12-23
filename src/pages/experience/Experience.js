5f09f17bec23578c57bf713d01ada03d3fd68a94
LoganJones <logan.b.jones16@gmail.com>
2020-12-23 14:37:00 -0800

    Add coding image

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -1,6 +1,53 @@
 import React from "react";
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography, Divider, Fab } from "@material-ui/core";
+import { Parallax } from "react-parallax";
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+
+import codingImage from './coding.png';
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center"
+  },
+  divider: {
+    margin: 30,
+  },
+  longText: {
+    maxWidth: 800,
+  },
+});
 
 export default function Experience() {
-  return <Typography  variant='h2'>EXPERIENCE</Typography>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <Parallax bgImage={codingImage}  strength={500}>
+          <div style={{ height: 600 }}>
+            <Grid 
+              style={{ height: "100%" }}
+              container 
+              direction='row-reverse' 
+              justify='flex-start' 
+              alignItems='flex-end'
+            >
+              <Grid item  style={{ margin: 30 }}>
+                <Fab  href="#start">
+                  <KeyboardArrowDown></KeyboardArrowDown>
+                </Fab>
+              </Grid>
+            </Grid>
+          </div>
+        </Parallax>
+      </div>
+      <div  id="start"></div>
+      <div>
+
+      </div>
+      <div style={{ height: 600 }}></div>
+      <div style={{ height: 600 }}></div>
+    </div>
+  );
 }
