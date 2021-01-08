778cbfe06f2fce05a85e341a1f5659ea82342895
LoganJones <logan.b.jones16@gmail.com>
2021-01-08 15:02:00 -0800

    Refactor StartImage to allow for more foreground elements

--- src/common/StartImage.js
+++ src/common/StartImage.js
@@ -1,31 +1,46 @@
 import React from "react";
-import { Grid, Fab } from "@material-ui/core";
+import { Grid, Fab, Box, makeStyles } from "@material-ui/core";
 import { Parallax } from "react-parallax";
 import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
 
-export default function StartImage(props) {
-    var clickHandler = props.onClick;
-    var bgImage = props.bgImage
-    var strength = props.strength || 500;
-    var height = props.height || window.innerHeight - 100;
-    height = Math.max(height, 100);
+const useStyles = makeStyles(() => ({
+  tallFill: {
+    height: "100%",
+  },
+}));
 
-    return (
-      <Parallax bgImage={bgImage} strength={strength}>
-        <div style={{ 'height': height }}>
-          <Grid
-            style={{ 'height': "100%" }}
-            container
-            direction='row-reverse'
-            justify='flex-start'
-            alignItems='flex-end'
-          >
-            <Grid item style={{ margin: 30 }}>
-              <Fab  onClick={clickHandler}>
-                <KeyboardArrowDown></KeyboardArrowDown>
-              </Fab>
-            </Grid>
+
+export default function StartImage(props) {
+  const classes = useStyles();
+  var clickHandler = props.onClick;
+  var bgImage = props.bgImage
+  var strength = props.strength || 500;
+  var height = props.height || window.innerHeight - 100;
+  height = Math.max(height, 100);
+
+  return (
+    <Parallax bgImage={bgImage} strength={strength}>
+      <div style={{ 'height': height }}>
+        <Grid
+          container
+          className={classes.tallFill}
+          direction='row'
+        >
+          <Grid item xs={3}>
+            {props.left}
           </Grid>
-        </div>
-      </Parallax>);
+
+          <Grid item xs={6}>
+            {props.center}
+          </Grid>
+          
+          <Grid container alignItems='flex-end'  justify='flex-end'>
+            <Fab  onClick={clickHandler}  style={{ margin: 30 }}>
+              <KeyboardArrowDown></KeyboardArrowDown>
+            </Fab>
+          </Grid>
+
+        </Grid>
+      </div>
+    </Parallax>);
   }
