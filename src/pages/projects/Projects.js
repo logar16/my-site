258269b153a46935b74a80d6372d6dec18a89454
LoganJones <logan.b.jones16@gmail.com>
2021-01-14 20:42:39 -0800

    Add LunarLander Project

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Divider, Grid, Typography } from "@material-ui/core";
+import { Divider, Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
 import Carousel from './DemoCarousel';
+import LunarLander from './LunarLanderProj'
 import Contact from '../../common/Contact';
 
 
 const useStyles = makeStyles(() => ({
+  list: {
+    width: '100%',
+  },
+  item: {
+    width: '100%',
+    padding: 50,
+    margin: '20 0'
+  },
+  paper: {
+    width: '100%',
+    minHeight: 100,
+    backgroundColor: '#2a2a2a'
+  },
+  projectItem: {
+    padding: 10
+  },
   divider: {
     margin: 30,
   },
@@ -17,15 +34,19 @@ const useStyles = makeStyles(() => ({
 export default function Projects() {
   const classes = useStyles();
 
-  const clickHandler = () => {
-    let start = document.getElementById('start');
-    start.scrollIntoView();
+  const handleClick = (event) => {
+    let id = event.target.id || '';
+    if (!id) return;
+    id = id.replace('-img', '');
+    let element = document.getElementById(id);
+    if (element)
+      element.scrollIntoView();
   }
 
   return (
     <div>
       <div>
-        <Carousel></Carousel>
+        <Carousel onClick={handleClick}></Carousel>
       </div>
       
       <div id='start' style={{height: 10}}/>
@@ -44,9 +65,20 @@ export default function Projects() {
 
 
 function ProjectList() {
+  const classes = useStyles();
+
   return (
-    <Grid>
-      
+    <Grid 
+      container
+      className={classes.list}
+      direction='column'
+      alignItems='center'
+    >
+      <Grid item  className={classes.item}>
+        <LunarLander/>
+      </Grid>
     </Grid>
   );
 }
+
+
