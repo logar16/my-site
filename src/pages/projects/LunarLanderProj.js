a8bc3559eeb0b1eda1d7508b59903ea441b3acad
LoganJones <logan.b.jones16@gmail.com>
2021-01-14 21:19:07 -0800

    Update LunarLander description

--- src/pages/projects/LunarLanderProj.js
+++ src/pages/projects/LunarLanderProj.js
@@ -77,10 +77,10 @@ function Body() {
             <Typography variant='body1'>
               I solved the OpenAI gym LunarLander-v2 environment with an average reward of 200+ for 100 test episodes.
               This is the stated goal of the environment according to the <Link color='secondary' href='https://gym.openai.com/envs/LunarLander-v2/' target='_blank' rel="noreferrer">official environment documentation</Link>.
-              I was able to achieve this within 400 episodes of training, viable to be on the official <Link color='secondary' href='https://github.com/openai/gym/wiki/Leaderboard#lunarlander-v2' target='_blank' rel="noreferrer">leaderboard</Link>.
+              I was able to achieve this within 400 episodes of training, meaning it is viable to be on the official <Link color='secondary' href='https://github.com/openai/gym/wiki/Leaderboard#lunarlander-v2' target='_blank' rel="noreferrer">leaderboard</Link>. Hmm...maybe I should add it.
               <br></br>
               <br></br>
-              I built my own DQN agent from scratch, following the original DQN paper released by DeepMind.  
+              For this project, I built my own DQN agent from scratch, following the original DQN paper released by DeepMind. I made it very performant using PyTorch for both memory storage and the agent.
               Training and evaluation metrics as well as additional details on the whole process can be found at the project repository:
               <br></br>
               <Link color='secondary' href='https://github.com/logar16/LunarLander' target='_blank' rel="noreferrer">https://github.com/logar16/LunarLander</Link>.
