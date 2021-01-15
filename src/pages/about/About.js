a04ac9bce2e486ac6a2e9613fdd265ec55be5402
LoganJones <logan.b.jones16@gmail.com>
2021-01-14 23:33:03 -0800

    Add About skeleton

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,6 +1,45 @@
 import React from "react";
 import Typography from '@material-ui/core/Typography';
+import { Divider } from "@material-ui/core";
+
+import Contact from "../../common/Contact";
+import StartImage from "../../common/StartImage";
+
+import codingImage from '../experience/coding.png';
 
 export default function About() {
-  return <Typography  variant='h2'>ABOUT</Typography>;
+
+  const clickHandler = () => {
+    let element = document.getElementById('start');
+    window.scroll(0, element.offsetTop - 50);
+  }
+
+  return (
+    <div>
+      <div>
+        <StartImage onClick={clickHandler} bgImage={codingImage}  strength={400}/>
+      </div>
+
+      <div  id='start'></div>
+
+      <div>
+        <Something/>
+      </div>
+
+      <div>
+        <Divider></Divider>
+        <Contact></Contact>
+      </div>
+    </div>
+  );
+}
+
+function Something() {
+  return (
+    <div>
+      <Typography>
+        What else is there?
+      </Typography>
+    </div>
+  );
 }
