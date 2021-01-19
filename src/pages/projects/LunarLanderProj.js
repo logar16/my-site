9d5c5b6847a3abe91fbacb0670674d3ef4ba26e8
LoganJones <logan.b.jones16@gmail.com>
2021-01-18 16:03:54 -0800

    Update projects with summaries

--- src/pages/projects/LunarLanderProj.js
+++ src/pages/projects/LunarLanderProj.js
@@ -12,26 +12,76 @@ export default function LunarLander() {
       title='Lunar Lander' 
       image={landingGif}
     >
-      <Description/>
+      <Body/>
     </Entry>
   );
 }
 
+function Body() {
+  return (
+    <div>
+      <Summary/>
+      <Description/>
+    </div>
+  );
+}
+
+function Summary() {
+  return (
+    <div>
+      <Typography variant='h4'>
+        Summary of Experience
+      </Typography>
+      <Typography variant='body1' component='div'>
+        <ul>
+          <li>Implemented a very competitive Deep Q-Network from scratch using only <Link href='http://files.davidqiu.com//research/nature14236.pdf' target='_blank' rel='noreferrer'>the published paper</Link> as reference.</li>
+          <li>Solved a control problem in a continuous state space with discrete actions (i.e. LunarLander-v2)</li>
+          <li>Gained practical PyTorch experience:
+            <ul>
+              <li>Wrote code to alternate between utilizing <code>cuda</code> or <code>cpu</code> based on available hardware and configuration files to achieve maximum performance on the given machine</li>
+              <li>Improved batch experience replay execution speed using Tensors and slicing/vectorization</li>
+              <li>Implemented both linear and recurrent neural networks (i.e. GRU and LSTM)</li>
+              <li>Formulated a Reinforcement Learning problem into a Supervised Learning problem to train the model</li>
+              <li>Gained additional experience with the training and optimizer update loop</li>
+            </ul>
+          </li>
+          <li>Practiced automated hyperparameter search to quickly find best hyperparameters for training</li>
+          <li>Thoroughly graphed and output metrics to enable quick and efficient discernment of training progress and success or failure of evaluation</li>
+          <li>Management of Python libraries through <code>conda</code> and <code>pip</code></li>
+        </ul>
+      </Typography>
+    </div>
+  );
+}
 
 function Description() {
   return (
-    <Typography variant='body1'>
-      I solved the OpenAI gym LunarLander-v2 environment with an average reward of 200+ for 100 test episodes.
-      This is the stated goal of the environment according to the <Link href='https://gym.openai.com/envs/LunarLander-v2/' target='_blank' rel="noreferrer">official environment documentation</Link>.
-      I was able to achieve this within 400 episodes of training, meaning it is viable to be on the official <Link href='https://github.com/openai/gym/wiki/Leaderboard#lunarlander-v2' target='_blank' rel="noreferrer">leaderboard</Link>. 
-      Hmm...maybe I <i>should</i> add it.
-      <br></br>
-      <br></br>
-      For this project, I built my own DQN agent from scratch, following the original DQN paper released by DeepMind. I made it very performant using PyTorch for both memory storage and the agent.
-      Training and evaluation metrics as well as additional details on the whole process can be found at the project repository:
-      <br></br>
-      <Link href='https://github.com/logar16/LunarLander' target='_blank' rel="noreferrer">https://github.com/logar16/LunarLander</Link>.
-      Feel free to take it for a spin! 😉
-    </Typography>
+    <div>
+      <Typography variant='h4'>
+        Details
+      </Typography>
+      <Typography variant='body1'>
+        I solved the OpenAI gym LunarLander-v2 environment with an average reward of 200+ for 100 test episodes.
+        This is the stated goal of the environment according to the <Link href='https://gym.openai.com/envs/LunarLander-v2/' target='_blank' rel="noreferrer">official environment documentation</Link>.
+        I was able to achieve this within 400 episodes of training, meaning it is viable to be on the official <Link href='https://github.com/openai/gym/wiki/Leaderboard#lunarlander-v2' target='_blank' rel="noreferrer">leaderboard</Link>. 
+        Hmm...maybe I <i>should</i> add it.
+        <br></br>
+        <br></br>
+        For this project, I built my own DQN agent from scratch, following the original DQN paper released by DeepMind. 
+        I made it very performant using PyTorch for both memory storage and the agent. I found PyTorch to be more performant than the Keras library I had initially attempted to use.
+        With PyTorch, I was able to see a 50% or so increase in training speed as it took advantage of my multi-core CPU and my NVIDIA GPU much more easily.
+        <br></br>
+        <br></br>
+        To find a good configuration, I hand-crafted my own hyperparameter search methods.  
+        I even automated training so that I could let my computer run for a day, searching for the best combination of several distinct hyperparameters and network architectures.
+        Along with the search, I implemented several graphs to help me track the change in loss (during training) and reward by episode (as well as rolling reward means and standard deviations). 
+        <br></br>
+        <br></br>
+        All in all, I felt like I learned a lot about how to (and how not to) build a Deep RL agent and its surrounding training code.
+        Training and evaluation metrics as well as additional details and instructions on how to run it yourself can be found at the project repository: <Link href='https://github.com/logar16/LunarLander' target='_blank' rel="noreferrer">https://github.com/logar16/LunarLander</Link>.
+        <br></br>
+        Feel free to take it for a spin! 😉
+      </Typography>
+    </div>
   );
 }
--- src/pages/projects/UnityMLAgentsProj.js
+++ src/pages/projects/UnityMLAgentsProj.js
@@ -12,32 +12,90 @@ export default function UnityMlAgents() {
       title="Unity's ML-Agents" 
       image={mlagentsImage}
     >
-      <Description/>
+      <div>
+        <Summary/>
+        <Details/>
+        <FutureWork/>
+      </div>
     </Entry>
   );
 }
 
-
-function Description() {
+function Summary() {
   return (
-    <Typography variant='body1'>
-      For my final project in my Deep Learning course from Georgia Tech, I worked with a team to build intelligent agents that could coordinate teams to play soccer (or football if you want to get picky about it).
-      I learned about Unity and their awesome toolkit for testing and training agents using Reinforcement Learning.
-      I've always been a fan of RL, so it was invigorating to learn that Unity is also part of the fan club.
-      Their API is more robust than the basic Gym API (although they are mostly compatible) and their support for multiple agents and teams etc. got me really excited.
-      Not only was I able to gain some great experience in Unity and with RL for my class, but I also was given inspiration on some personal projects I want to pursue.
+    <div>
+      <Typography variant='h4'>
+        Summary
+      </Typography>
+      <Typography variant='body1' component='div'>
+        <ul>
+          <li>Experience with Unity's ML-Agents toolkit and API</li>
+          <li>Created and modified C# scripts to enhance learning and evaluation of performance
+            <ul>
+              <li>For instance, added reward shaping to reduce the number of steps to convergence</li>
+            </ul>
+          </li>
+          <li>Familiarized with the ML-Agents documentation and its nuances
+            <ul>
+              <li>Improved team's use of the API and CLI to best use parallelization and reduced training by <b>75%</b></li>
+            </ul>
+          </li>
+          <li>Used Unity as an editor to build Prefabs, Scenes, and RL environments</li>
+          <li>Learned how to use the ML-Agents toolkit to allow for easy environment creation and agent training</li>
+        </ul>
+      </Typography>
+    </div>
+  );
+}
+
+function Details() {
+  return (
+    <div>
+      <Typography variant='h4'>
+        Details
+      </Typography>
+      <Typography variant='body1'>
+        For my final project in my Deep Learning course at Georgia Tech, I worked with a team to build intelligent agents that could coordinate teams to play soccer (or "football" if you want to get picky about it).
+        I learned about Unity and their awesome toolkit for testing and training agents using Reinforcement Learning.
+        I've always been a fan of RL, so it was invigorating to learn that Unity is also part of the fan club.
+        Their API is more robust than the basic Gym API and their support for multiple agents and teams etc. got me really excited, as I am convinced that we need to see more multi-agent RL research.
+        Not only was I able to gain some great experience in Unity and with RL for my class, but I also was given inspiration on some personal projects I want to pursue.
+        <br></br>
+        <br></br>
+        For the final project I manned creating/modifying Unity assets such as the prefabs, scenes, and scripts.
+        I also took charge of evaluating and comparing different models over 1,000 games for each match-up.  
+        I also carved the way for the team by experimenting both with the CLI (letting ML-Agents code do training) and by interfacing with the API directly.  
+        We chose to stick with the former, but now I know how to do it either way, so I can train either simple or complex models as needed. 
+      </Typography>
+    </div>
+  );
+}
+
+function FutureWork() {
+  return (
+    <div>
       <br></br>
-      <br></br>
-      First is creating an environment similar to (but more complicated than) the Food Collection environment.
-      The repository can be found here: <Link href='https://github.com/logar16/ScavengerWorldUnity' target='_blank' rel="noreferrer">https://github.com/logar16/ScavengerWorldUnity</Link>.
-      I envision it having several teams of simple units that must work cooperatively to explore and exploit the environment.
-      I am inspired by biology and the emergence of complex behaviors in colonial animals (e.g. ants and bees) due to very simple rules which synergistically combine.
-      I hope to elicit similar organic emergence using deep learning and an environment which encourages cooperation <i>and</i> competition instead of only one or the other.
-      I will provide methods for communication, such as leaving distinct markers, to see if the agents come up with any new and interesting communication protocols.
-      <br></br>
-      <br></br>
-      Secondly, I am quite intrigued by the game <i>1313 Dead End Drive</i> which involves much scheming and tough choices of when to move one's own characters or another's.
-      I want to build a simplified simulator of the game which will follow the ML-Agent's API so that I can find an optimal solution to this fun but difficult game.
-    </Typography>
+      <Typography variant='h4'>
+        Future Directions
+      </Typography>
+      <Typography>
+        Now that I've got a handle on how to use ML-Agents, I want to do some personal research and projects.
+        <br></br>
+        <br></br>
+        First is creating an environment similar to (but more complicated than) the Food Collection environment.
+        The repository can be found here: <Link href='https://github.com/logar16/ScavengerWorldUnity' target='_blank' rel="noreferrer">https://github.com/logar16/ScavengerWorldUnity</Link>.
+        I envision it having several teams of simple units that must work cooperatively to explore and exploit the environment.
+        I am inspired by biology and the emergence of complex behaviors in colonial animals (e.g. ants and bees) due to very simple rules which synergistically combine.
+        I hope to elicit similar organic emergence using deep learning and an environment which encourages cooperation <i>and</i> competition instead of only one or the other.
+        I will provide methods for communication, such as leaving distinct markers, to see if the agents come up with any new and interesting communication protocols.
+        It is still in the early stages, but I will be working on it as I have time.
+        <br></br>
+        <br></br>
+        Secondly, I am quite intrigued by the game <i>1313 Dead End Drive</i> which involves much scheming and tough choices of when to move one's own characters or another's.
+        I want to build a simplified simulator of the game which will follow the ML-Agent's API so that I can find an optimal solution to this fun but difficult game.
+        Since the game requires predicting or simulating other agent's model of the world and possible decision making process, it is of similar difficulty to the challenging game of Hanabi which was picked out by <Link href='https://arxiv.org/abs/1902.00506' target='_blank' rel='noreferrer'>researchers at DeepMind</Link> as an important problem to solve.
+        I think this (my proposed environment) would also serve as a useful testing environment for RL algorithms but in a competitive environment.
+      </Typography>  
+    </div>
   );
 }
