33726f4c09da417e50a06c1f577c54ded4947d9a
LoganJones <logan.b.jones16@gmail.com>
2021-01-20 00:31:44 -0800

    Add OtherPagesPortal Portal = Button Cards to direct to other pages

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography, Divider, Link } from "@material-ui/core";
+import { Grid, Typography, Divider, Link, Hidden, Container } from "@material-ui/core";
 
-import StartImage from '../../common/StartImage'
-import Contact from '../../common/Contact'
-import {EducationExperience, WorkExperience} from './ExperienceSummary'
+import StartImage from '../../common/StartImage';
+import Contact from '../../common/Contact';
+import {EducationExperience, WorkExperience} from './ExperienceSummary';
+import ButtonGrid from './OtherPagesPortal';
 
 import portrait from './profile.jpg';
 
 const useStyles = makeStyles({
-  root: {
-    textAlign: "center"
-  },
   divider: {
     margin: 30,
   },
   longText: {
     maxWidth: 800,
+    marginTop: 10,
+    marginBottom: 10
   },
   experiences: {
     maxWidth: 1200,
     margin: 'auto'
-  }
+  },
 });
 
 export default function Home() {
@@ -43,12 +43,15 @@ export default function Home() {
         />
       </div>
       <div  ref={startRef}></div>
-      <div>
+      <Container>
         <Divider className={classes.divider} variant='middle'></Divider>
 
         <Grid container justify="center">
           <Grid item>
-            <Typography className={classes.longText} variant="body1">{elevatorPitch}</Typography>
+            <Typography 
+              className={classes.longText} variant="body1" align='justify'>
+                {elevatorPitch}
+              </Typography>
           </Grid>
         </Grid>
 
@@ -58,6 +61,10 @@ export default function Home() {
 
         <Divider  className={classes.divider} variant='middle'></Divider>
 
+        <ButtonGrid/>
+
+        <Divider  className={classes.divider} variant='middle'></Divider>
+        
         <Grid container justify="center">
           <Grid item>
             <Invitation className={classes.longText}/>
@@ -66,8 +73,8 @@ export default function Home() {
 
         <Divider  className={classes.divider} variant='middle'></Divider>
 
-        <Contact></Contact>
-      </div>
+        <Contact/>
+      </Container>
     </div>
   );
 }
@@ -75,11 +82,13 @@ export default function Home() {
 function Intro() {
   return (
     <Grid container justify='center' alignItems='flex-end' style={{height: '100%'}}>
-      <Typography className='rotated' variant='h4'>
-        Hello!
-        <br></br>
-        I am Logan Jones
-      </Typography>
+      <Hidden xsDown>
+        <Typography className='rotated' variant='h4'  align='center'>
+          Hello!
+          <br></br>
+          I'm Logan
+        </Typography>
+      </Hidden>
     </Grid>
   );
 }
@@ -89,12 +98,12 @@ function Experiences() {
   const classes = useStyles();
   return (
     <div  className={classes.experiences}>
-      <Typography variant="h3">Education</Typography>
+      <Typography variant="h3"  align='center'>Education</Typography>
       <EducationExperience/>
 
       <Divider  className={classes.divider} variant='middle'></Divider>
 
-      <Typography variant="h3">Work Experience</Typography>
+      <Typography variant="h3"  align='center'>Work Experience</Typography>
       <WorkExperience/>
     </div>
   );
@@ -102,23 +111,30 @@ function Experiences() {
 
 
 const elevatorPitch = `My name is Logan Jones. I'm a self-taught programmer with a Master’s degree in 
-Artificial Intelligence and Machine Learning and over four years of 
-work experience as a Software Engineer. 
-I'm trying to break into a new role as a ML researcher and engineer.  I love
-Reinforcement Learning and making AI agents that can make decisions on their own.`
+Artificial Intelligence and Machine Learning. Over the last four years I've been a working as a Software Engineer. 
+Now I'm trying to break into a new role as a ML researcher and engineer.  
+I love Reinforcement Learning and making AI agents that can make decisions on their own!`
 
 
 function Invitation(props) {
   return (
-    <Typography className={props.className}>
-      Check out the other sections of the site to learn more about my <Link href='/#/experience/gatech?switch=true'>education</Link>
-      , <Link href='/#/experience/amazon?switch=true'>work experience</Link>
-      , <Link href='/#/projects'>ongoing projects</Link>, 
-      and <Link href='/#/about'>general information</Link> about me and how I got here.
-      You can also find ways to contact me below. I'm always happy to connect! 
-      I am looking for remote positions at this time.  If you or someone you know wants to hire someone like me, please let me know!  
-      <br></br>
-      Cheers!
-    </Typography>
+    <div  style={{marginTop: 30}}>
+      <Typography align='justify' className={props.className}>
+        You can find ways to contact me below. I'm always happy to connect with other humans! 
+        I am looking for ML research and engineering positions at this time, and 100% remote if possible.  
+        If you or someone you know wants to hire someone like me, please let me know!  Until then,
+      </Typography>
+      {/* <Typography align='justify' className={props.className}>
+        Check out the other sections of the site to learn more about my <Link href='my-site/#/experience/gatech?switch=true'>education</Link>
+        , <Link href='my-site/#/experience/amazon?switch=true'>work experience</Link>
+        , <Link href='my-site/#/projects'>ongoing projects</Link>, 
+        and <Link href='my-site/#/about'>general information</Link> about me and how I got here.
+        You can also find ways to contact me below. I'm always happy to connect! 
+        I am looking for remote positions at this time.  If you or someone you know wants to hire someone like me, please let me know!  
+      </Typography> */}
+      <Typography align='center'>
+        Cheers!
+      </Typography>
+    </div>
   );  
 }
---
+++ src/pages/home/OtherPagesPortal.js
@@ -0,0 +1,85 @@
+
+import { Grid, Typography, makeStyles, Card, CardContent, CardActionArea, CardMedia, Box } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+import expImage from '../experience/coding.png'
+import projImage from '../projects/ML-Agents-Banner.png'
+import aboutImage from './profile-thin.jpg'
+
+
+const useStyles = makeStyles({
+  root: {
+    padding: 10,
+  },
+  card: {
+    height: '100%',
+  },
+  image: {
+    height: 200,
+  }
+});
+
+
+export default function PortalGrid() {
+  const classes = useStyles();
+  
+  return (
+    <Grid 
+      container 
+      justify='space-around'
+      spacing={3} 
+      className={classes.root}
+    >
+      <Item title='Experience'  link='/experience'  img={expImage}>
+        My educational background and work experience.  If you want to know what I know, look here
+      </Item>
+      <Item title='Projects'  link='/projects'  img={projImage}>
+        Projects I am working on to stay sharp outside of work and school
+      </Item>
+      <Item title='About' link='/about' img={aboutImage}>
+        Information about me on a more personal level
+      </Item>
+    </Grid>
+  );
+}
+
+function Item({title, link, img, children}) {
+  return (
+    <Grid item  xs={8} md={6} lg={4}>
+      <GridCard title={title} link={link} img={img}>
+        {children}
+      </GridCard>
+    </Grid>
+  );
+}
+
+function GridCard({title, link, img, children}) {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(link);
+  }
+
+  console.log(img)
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea className={classes.card}  onClick={handleClick}>
+        <CardMedia  image={img} className={classes.image}>
+          {/* <img src={img} className={classes.image} /> */}
+        </CardMedia>
+        <CardContent  className={classes.card}>
+          <Grid container direction='column' justify='start'>
+            <Typography variant='h4'  align='center'>
+              {title}
+            </Typography>
+            <Typography variant='body1' color='textSecondary'>
+              {children}
+            </Typography>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
