4d96bcb77efc982d0b909092de0a57e4aebb0543
LoganJones <logan.b.jones16@gmail.com>
2021-01-20 00:29:33 -0800

    Refactor Home's experience summaries to be responsive buttons

--- src/pages/home/ExperienceSummary.js
+++ src/pages/home/ExperienceSummary.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Link, Paper, Typography } from "@material-ui/core";
+import { Button, Container, Grid, Link, Paper, Typography } from "@material-ui/core";
 
 import gatechImage from '../experience/gatech.png'
 import suuImage from '../experience/suu.png'
@@ -10,18 +10,27 @@ import cgmImage from '../experience/cgm.png'
 
 const useStyles = makeStyles({
   logos: {
-    maxWidth: 400,
-    maxHeight: 300,
+    maxWidth: '98%',
+    maxHeight: '98%',
     backgroundColor: '#eeeeee'
   },
   row: {
-    padding: "5% 10% 5%",
+    padding: "3% 3% 1%",
+  },
+  item: {
+    backgroundColor: '#333333'
+  },
+  moreLink: {
+    paddingBottom: 5
+  },
+  button: {
+    textTransform: 'none'
   }
 });
 
 export function EducationExperience() {
   return (
-    <Grid container direction="column" justify="space-evenly">
+    <Grid container direction="column" justify="space-around" spacing={2}>
       <ExperienceSummary   experience={gatech}/>
       <ExperienceSummary   experience={suu}/>
     </Grid>
@@ -31,7 +40,7 @@ export function EducationExperience() {
 
 export function WorkExperience() {
   return (
-    <Grid container direction="column" justify="space-evenly">
+    <Grid container direction="column" justify="space-around" spacing={2}>
       <ExperienceSummary  experience={amazon}/>
       <ExperienceSummary  experience={cgm}/>
     </Grid>
@@ -43,21 +52,34 @@ function ExperienceSummary({experience}) {
   const classes = useStyles();
   return (
     <Grid item>
-        <Grid 
-          container 
-          justify="center"
-          alignItems="center"
-          className={classes.row}
-        >
-          <Grid item  xs={6}>
-            <Paper  className={classes.logos}>
-              <img style={{width: '100%', height: '100%'}} src={experience.image} alt=""/>
-            </Paper>
+      <Button className={classes.button} href={experience.link}>
+        <Paper className={classes.item}>
+          <Grid 
+            container 
+            justify="center"
+            alignItems="center"
+            className={classes.row}
+          >
+            <Grid item  xs={12} md={6}>
+              <Paper  className={classes.logos}>
+                <img style={{width: '100%', height: '100%'}} src={experience.image} alt=""/>
+              </Paper>
+            </Grid>
+            
+            <Grid item xs={1} md={1}></Grid>
+
+            <Grid item xs={12} md={5}>
+              <ExperienceText>{experience}</ExperienceText>
+            </Grid>
           </Grid>
-          <Grid item  xs={6}>
-            <ExperienceText>{experience}</ExperienceText>
-          </Grid>
-        </Grid>
+
+
+          <Typography variant='body2' align='center' color='textSecondary' className={classes.moreLink}>
+            <i>click for more details</i>
+            {/* <Link href={experience.link}>More</Link> */}
+          </Typography>
+        </Paper>
+      </Button>
     </Grid>
   );
 }
@@ -66,26 +88,25 @@ function ExperienceText(props) {
   let experience = props.children;
 
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <Typography variant="h4">
-          {experience.title}
-        </Typography>
+    <Container style={{textAlign: 'left'}}>
+      <Grid container direction="column"  alignItems='stretch'>
+        <Grid item>
+          <Typography variant="h4">
+            {experience.title}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h6"  style={{whiteSpace: "pre-line"}}>
+            {experience.subtitle}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h6"  style={{whiteSpace: "pre-line"}}>
+            {experience.details}
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h6" style={{whiteSpace: "pre-line"}}>
-          {experience.subtitle}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Typography variant="h6">
-          {experience.details}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Link href={experience.link}>More</Link>
-      </Grid>
-    </Grid>
+    </Container>
   );
 }
 
@@ -101,14 +122,14 @@ function Experience(title, subtitle, details, link, image) {
 const gatech = new Experience(
   "Master of Science",
   "Major: Computer Science\nConcentration: Interactive Intelligence",
-  "Graduated Dec 2020 with GPA 3.80",
+  "Graduated Dec 2020\nGPA 3.80",
   "gatech",
   gatechImage
 );
 const suu = new Experience(
   "Bachelor of Science",
   "Major: Biology\nMinor: Chemistry",
-  "Graduated May 2016 with GPA 3.77",
+  "Graduated May 2016\nGPA 3.77",
   "suu",
   suuImage
 );
